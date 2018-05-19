2f6eae663359825fd35fb79af7e8c50172207b73
dewie102 <zackariah.romasz@yahoo.com>
2018-05-18 22:54:37 -0400

    Finished first exercise, still having issues with &e, waiting for response from instructor.

--- Exceptions/Basic Exceptions.cpp
+++ Exceptions/Basic Exceptions.cpp
@@ -2,8 +2,37 @@
 
 using namespace std;
 
+double ShouldGoWrong (double x, double y) {
+	if (y == 0.0) {
+		throw -1;
+	}
+
+	if (x == -1.0) {
+		throw "Divided into -1, just testing it works";
+	}
+
+	if (x + y == 10.0) {
+		throw string ("testing string but this is a char for some reason...");
+	}
+
+	return x / y;
+}
+
 int main () {
-	
+	double result = 0.0;
+	try {
+		result = ShouldGoWrong (5.0, 5.0);
+	} catch (int e) {
+		if (e == -1) {
+			cout << "Error Occurred: " << e << "...\nDivided by zero." << endl;
+		}
+	} catch (char const * e) {
+		cout << "Error Occurred: " << e << endl;
+	} catch (string &e) {
+		cout << "Need help now... " << e.c_str() << endl;
+	}
+
+	cout << "result: " << result << endl;
 
 	return 0;
 }
