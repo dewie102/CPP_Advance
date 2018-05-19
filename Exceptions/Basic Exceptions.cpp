a3e737eb9b40168ca664074aa29e6f3c47e028be
dewie102 <zackariah.romasz@yahoo.com>
2018-05-18 23:29:57 -0400

    Bad_Alloc exception. Didn't work on VS because it automatically catches allocation of too much memory. Not going to try exercise because of that.

--- Exceptions/Basic Exceptions.cpp
+++ Exceptions/Basic Exceptions.cpp
@@ -2,34 +2,22 @@
 
 using namespace std;
 
-void mightGoWrong () {
-	bool error1 = false;
-	bool error2 = true;
-
-	if (error1) {
-		throw "Something went wrong.";
+class CanGoWrong {
+public:
+	CanGoWrong () {
+		char *pMemory = new char[999999999];
+		delete[] pMemory;
 	}
-
-	if (error2) {
-		throw string("Something else went wrong.");
-	}
-}
-void usesMightGoWrong () {
-	mightGoWrong ();
-}
+};
 
 int main () {
 	try {
-		usesMightGoWrong ();
-	} catch (int e) {
-		cout << "Error Code: " << e << endl;
-	} catch (char const * e) {
-		cout << "Error Code: " << e << endl;
-	} catch (string &e) {
-		cout << "String Error Code: " << &e << endl;
+		CanGoWrong wrong;
+	} catch (bad_alloc &e) {
+		cout << "Caught exception: " << e.what () << endl;
 	}
 
-	cout << "Still Running" << endl;
+	cout << "still running" << endl;
 
 	return 0;
 }
